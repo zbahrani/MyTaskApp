4f6b729866b067597dd58c01345efce4d931f39e
ACER <ACER@DESKTOP-AO608KQ>
2025-09-17 22:54:18 +0330

    add IProductService interface with query and analysis methods.

---
+++ MyTaskApp/Services/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using MyTaskApp.Model;
+using System.Collections.Generic;
+
+namespace MyTaskApp.Services
+{
+    public interface IProductService
+    {
+        List<Product> GetByCategory();
+        public Product GetProductWithMaxPrice();
+        public decimal GetTotalPrice();
+        public decimal GetAveragePrice();
+        Dictionary<Categories, List<Product>> GetProductsGroupedByCategory();
+
+    }
+}

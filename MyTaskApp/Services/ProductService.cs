7c73a1d9cc3e92661eda6a8e72fcb2645b43085c
ACER <ACER@DESKTOP-AO608KQ>
2025-09-17 23:25:00 +0330

    implement ProductServiceBase class inheriting IProductService and Add GetProductWithMaxPrice, GetTotalPrice methods, ...

---
+++ MyTaskApp/Services/ProductService.cs
@@ -0,0 +1,30 @@
+﻿using MyTaskApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaskApp.Services
+{
+    public class ProductService : ProductServiceBase
+    {
+        public ProductService(List<Product> products) : base(products)
+        {
+        }
+
+        public override List<Product> GetByCategory()
+        {
+            return _products
+                .Where(p => p.Category == Categories.Category1)
+                .ToList();
+        }
+
+        public override Product GetProductWithMaxPrice()
+        {
+            return _products
+                .OrderByDescending(p => p.Price)
+                .FirstOrDefault();
+        }
+    }
+}
---
+++ MyTaskApp/Services/ProductServiceBase.cs
@@ -0,0 +1,33 @@
+﻿using MyTaskApp.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyTaskApp.Services
+{
+    public abstract class ProductServiceBase : IProductService
+    {
+        protected readonly List<Product> _products;
+        protected ProductServiceBase(List<Product> products)
+        {
+            _products = products;
+        }
+        public abstract List<Product> GetByCategory();
+        public abstract Product GetProductWithMaxPrice();
+     
+        public virtual decimal GetAveragePrice()
+        {
+            return _products.Average(p => p.Price);
+        }
+
+        public virtual Dictionary<Categories, List<Product>> GetProductsGroupedByCategory()
+        {
+            return _products.GroupBy(p => p.Category)
+                .ToDictionary(g => g.Key, g => g.ToList());
+        }
+
+        public virtual decimal GetTotalPrice()
+        {
+            return _products.Sum(p => p.Price);
+        }
+    }
+}

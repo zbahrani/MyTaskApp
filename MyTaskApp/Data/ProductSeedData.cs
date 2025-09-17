2d1c84ee072e7af29413a367b5cd834e03e53c4b
ACER <ACER@DESKTOP-AO608KQ>
2025-09-17 22:40:08 +0330

    add ProductSeedData class for sample products.

---
+++ MyTaskApp/Data/ProductSeedData.cs
@@ -0,0 +1,20 @@
+﻿using MyTaskApp.Model;
+using System.Collections.Generic;
+
+namespace MyTaskApp.Data
+{
+    public static class ProductSeedData
+    {
+        public static List<Product> GetProducts()
+        {
+            return new List<Product>
+            {
+                new Product { Id = 1, Name = "Product A", Category = Categories.Category1, Price = 100 },
+                new Product { Id = 2, Name = "Product B", Category = Categories.Category1, Price = 150 },
+                new Product { Id = 3, Name = "Product C", Category = Categories.Category2, Price = 120 },
+                new Product { Id = 4, Name = "Product D", Category = Categories.Category3, Price = 200 },
+                new Product { Id = 5, Name = "Product E", Category = Categories.Category1, Price = 80 }
+            };
+        }
+    }
+}
